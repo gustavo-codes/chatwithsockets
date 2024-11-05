688237b0ca87dd634c84e4f59145c7cefb3eb358
gustavo-codes <reginaldo.gustavo2003@gmail.com>
2024-11-05 11:34:13 -0300

    attempt 1

--- tcpclient.py
+++ tcpclient.py
@@ -1,18 +1,28 @@
 from socket import *
+import threading
 
 HOST = "127.0.0.1"
 PORT = 50007
 
+def recv ():
+    while 1:
+        res = serverSocket.recv(1024).decode()#Bloqueia a exec até receber
+        print("server: " + res)
+
 serverSocket = socket()
 serverSocket.connect((HOST,PORT))
+rcvThread = threading.Thread(target=recv)
+rcvThread.start()
+
 while 1:
     msg = input('')
     
     if msg == "exit":
         serverSocket.close()
+        break
 
     serverSocket.send(msg.encode('utf-8'))
-    res = serverSocket.recv(1024).decode()#Bloqueia a exec até receber
-    print("server: " + res)
+      
+    
     
 
--- tcpserver.py
+++ tcpserver.py
@@ -4,6 +4,11 @@ import threading
 HOST = "127.0.0.1"
 PORT = 50007
 
+def recv ():
+    while 1:
+        req = connectionSocket.recv(1024).decode('utf-8')#Bloqueia a exec até receber
+        print("client: " + req)
+
 serverSocket = socket(AF_INET,SOCK_STREAM) #AF_INET IPV4, SOCK_STREAM TCP
 serverSocket.bind(('',PORT))
 serverSocket.listen(1)
@@ -11,10 +16,10 @@ while 1:
     connectionSocket,addr = serverSocket.accept()
     print("Connected to: ")
     print(addr)
+
+    rcvThread = threading.Thread(target=recv)
+    rcvThread.start()
     while 1:
-        req = connectionSocket.recv(1024).decode('utf-8')#Bloqueia a exec até receber
-        print("client: " + req)
-        
         res = input("")
 
         if res == "exit":
